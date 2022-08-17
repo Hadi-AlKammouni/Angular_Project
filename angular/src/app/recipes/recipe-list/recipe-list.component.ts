ee832f52cd624b355fe829225706fac80f26a5ec
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-08-17 16:50:32 +0300

    angular/Displaying recipe-list

--- angular/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('First test recipe', 'This is 1st the description', 'https://th.bing.com/th/id/R.89b0b4650f29a17030bad22c9e883f61?rik=293O790OghSJ5Q&pid=ImgRaw&r=0'),
+    new Recipe('Second test recipe', 'This is 2nd the description', 'https://th.bing.com/th/id/OIP.iWkh-Mq0JeIc7Nb8yzxShgHaHa?w=186&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7'),
+    new Recipe('Third test recipe', 'This is 3rd the description', 'https://th.bing.com/th/id/OIP.QTp2z7eTVzdqtP0kmh7DwgHaHa?w=186&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7')
+  ];
 
   constructor() { }
 
