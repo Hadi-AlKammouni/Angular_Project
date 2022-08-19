cf34148bd38aa46ad6f407ef64cc9614bc6fcd53
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-08-19 15:49:33 +0300

    angular/Closing the dropdown upon clicking anywhere outside

--- angular/src/app/shared/dropdown.directive.ts
+++ angular/src/app/shared/dropdown.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, HostBinding, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core";
 
 @Directive({
     selector: '[appDropdown]'
@@ -8,8 +8,16 @@ export class DropdownDirective {
     
     @HostBinding('class.open') isOpen = false;
     
-    @HostListener('click') toggleOpen() {
-        this.isOpen = !this.isOpen
+    // Close only when re-click on it
+    // @HostListener('click') toggleOpen() {
+    //     this.isOpen = !this.isOpen
+    // }
+
+    // Close upon clicking anywhere outside
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+        this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
     }
-    
+
+    constructor(private elRef: ElementRef) {}
+
 } 
