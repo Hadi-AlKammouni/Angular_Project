07929c1ecfaee83162dd3ee97519795954132103
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-08-20 15:49:25 +0300

    angular/Displaying ingredients of the recipe

--- angular/src/app/recipes/recipe.service.ts
+++ angular/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -6,9 +7,24 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('First test recipe', 'This is the 1st description', 'https://th.bing.com/th/id/R.89b0b4650f29a17030bad22c9e883f61?rik=293O790OghSJ5Q&pid=ImgRaw&r=0'),
-        new Recipe('Second test recipe', 'This is the 2nd description', 'https://th.bing.com/th/id/OIP.iWkh-Mq0JeIc7Nb8yzxShgHaHa?w=186&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7'),
-        new Recipe('Third test recipe', 'This is the 3rd description', 'https://th.bing.com/th/id/OIP.QTp2z7eTVzdqtP0kmh7DwgHaHa?w=186&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7')
+        new Recipe(
+            'Tasty', 
+            'A super tasty plate', 
+            'https://th.bing.com/th/id/R.89b0b4650f29a17030bad22c9e883f61?rik=293O790OghSJ5Q&pid=ImgRaw&r=0',
+            [new Ingredient('Meat',1), new Ingredient('French Fries',15)]
+        ),
+        new Recipe(
+            'Delicious', 
+            'Wow!', 
+            'https://th.bing.com/th/id/OIP.iWkh-Mq0JeIc7Nb8yzxShgHaHa?w=186&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7',
+            [new Ingredient('Cheese',4), new Ingredient('Juice',1)]
+        ),
+        new Recipe(
+            'Fantastic', 
+            'Very good', 
+            'https://th.bing.com/th/id/OIP.QTp2z7eTVzdqtP0kmh7DwgHaHa?w=186&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7',
+            [new Ingredient('Ketchup',1)]
+        )
     ];
 
     getRecipe() {
