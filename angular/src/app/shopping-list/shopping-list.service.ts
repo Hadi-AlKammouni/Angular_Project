f17dfc2c2de645e57f340a4dc6ae71d389039a3a
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-08-20 16:03:59 +0300

    angular/Passing ingredients from recipes to shopping list

--- angular/src/app/shopping-list/shopping-list.service.ts
+++ angular/src/app/shopping-list/shopping-list.service.ts
@@ -3,19 +3,33 @@ import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
 
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  
+  private ingredients: Ingredient[] =[
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomato', 10)
+  ];
 
-    private ingredients: Ingredient[] =[
-        new Ingredient('Apples', 5),
-        new Ingredient('Tomato', 10)
-      ];
+  getIngredientd() {
+    return this.ingredients.slice()
+  }
 
-    getIngredientd() {
-      return this.ingredients.slice()
-    }
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient)
+    this.ingredientsChanged.emit(this.ingredients.slice())
+  }
 
-    addIngredient(ingredient: Ingredient) {
-        this.ingredients.push(ingredient)
-        this.ingredientsChanged.emit(this.ingredients.slice())
-    }
+  // // First approach
+  // addIngredients(ingredients: Ingredient[]) {
+  //   for (let ingredient of ingredients) {
+  //     this.addIngredient(ingredient)
+  //   }
+  // }
+
+  // Second approach
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients)
+    this.ingredientsChanged.emit(this.ingredients.slice())
+  }
+
 }
