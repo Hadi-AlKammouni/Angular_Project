e8fc44c90f41cfc28bc7c068483e1b414b5b94c1
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-08-23 17:46:53 +0300

    angular/Adding form to the shopping list

--- angular/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ angular/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-// import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 
@@ -10,21 +11,14 @@ import { ShoppingListService } from '../shopping-list.service';
 })
 export class ShoppingEditComponent implements OnInit {
 
-  @ViewChild('nameInput') nameInputRef!: ElementRef;
-  @ViewChild('amountInput') amountInputRef!: ElementRef;
-  // @Output() ingredientAdded = new EventEmitter<{name: string, amount: number}>();
-  // @Output() ingredientAdded = new EventEmitter<Ingredient>();
-
   constructor(private shoppingListSercvice: ShoppingListService) { }
 
   ngOnInit(): void {
   }
 
-  onAddItem() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount)
-    // this.ingredientAdded.emit(newIngredient)
+  onAddItem(form: NgForm) {
+    const value = form.value
+    const newIngredient = new Ingredient(value.name, value.amount)
     this.shoppingListSercvice.addIngredient(newIngredient)
   }
 
