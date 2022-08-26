ed8fad66e06133706e3b9c3b4b79b651b96bef0f
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-08-26 19:23:19 +0300

    angular/Adding loading spinner + Handling an error

--- angular/src/app/auth/auth.component.ts
+++ angular/src/app/auth/auth.component.ts
@@ -8,6 +8,9 @@ import { AuthService } from "./auth.service";
 })
 export class AuthComponent {
     isLoginMode = true
+    isLoading = false
+    error = '';
+    
 
     constructor(private authService: AuthService) {}
 
@@ -22,13 +25,17 @@ export class AuthComponent {
         const email = form.value.email
         const password = form.value.password
 
+        this.isLoading = true
         if (this.isLoginMode) {
 
         } else {
             this.authService.signUp(email, password).subscribe(resData => {
                 console.log(resData)
+                this.isLoading = false
             }, error => {
                 console.log(error)
+                this.error = 'An error occured!'
+                this.isLoading = false
             });
         }
         
