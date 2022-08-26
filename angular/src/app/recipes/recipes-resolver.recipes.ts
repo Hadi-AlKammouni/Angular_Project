00eaedf6796f3c77642ec59dd7ab0724972405ec
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-08-26 11:00:13 +0300

    angular/Fixing edit recipe in the resolver to save the edit

--- angular/src/app/recipes/recipes-resolver.recipes.ts
+++ angular/src/app/recipes/recipes-resolver.recipes.ts
@@ -3,14 +3,21 @@ import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/r
 
 import { Recipe } from "./recipe.model";
 import { DataStorageService } from "../shared/data-storage.service";
+import { RecipeService } from "./recipe.service";
 
 @Injectable({providedIn: 'root'})
 export class RecipesResolverService implements Resolve<Recipe[]>{
     
-    constructor(private dataStorageServcie: DataStorageService) {}
+    constructor(private dataStorageServcie: DataStorageService, private recipeService: RecipeService) {}
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        return this.dataStorageServcie.fetchRecipes()
+        const recipes = this.recipeService.getRecipes()
+
+        if (recipes.length === 0) {
+            return this.dataStorageServcie.fetchRecipes()
+        } else {
+            return recipes
+        }
     }
 
 }
