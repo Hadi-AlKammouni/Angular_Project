ee19c01d71d0dd0767dcf3279d15fa8ba1be5d1c
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-08-26 10:41:51 +0300

    angular/Tranforming response data

--- angular/src/app/recipes/recipe.service.ts
+++ angular/src/app/recipes/recipe.service.ts
@@ -10,26 +10,28 @@ export class RecipeService {
 
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Tasty', 
-            'A super tasty plate', 
-            'https://th.bing.com/th/id/R.89b0b4650f29a17030bad22c9e883f61?rik=293O790OghSJ5Q&pid=ImgRaw&r=0',
-            [new Ingredient('Meat',1), new Ingredient('French Fries',15)]
-        ),
-        new Recipe(
-            'Delicious', 
-            'Wow!', 
-            'https://th.bing.com/th/id/OIP.iWkh-Mq0JeIc7Nb8yzxShgHaHa?w=186&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7',
-            [new Ingredient('Cheese',4), new Ingredient('Juice',1)]
-        ),
-        new Recipe(
-            'Fantastic', 
-            'Very good', 
-            'https://th.bing.com/th/id/OIP.QTp2z7eTVzdqtP0kmh7DwgHaHa?w=186&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7',
-            [new Ingredient('Ketchup',1)]
-        )
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'Tasty', 
+    //         'A super tasty plate', 
+    //         'https://th.bing.com/th/id/R.89b0b4650f29a17030bad22c9e883f61?rik=293O790OghSJ5Q&pid=ImgRaw&r=0',
+    //         [new Ingredient('Meat',1), new Ingredient('French Fries',15)]
+    //     ),
+    //     new Recipe(
+    //         'Delicious', 
+    //         'Wow!', 
+    //         'https://th.bing.com/th/id/OIP.iWkh-Mq0JeIc7Nb8yzxShgHaHa?w=186&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7',
+    //         [new Ingredient('Cheese',4), new Ingredient('Juice',1)]
+    //     ),
+    //     new Recipe(
+    //         'Fantastic', 
+    //         'Very good', 
+    //         'https://th.bing.com/th/id/OIP.QTp2z7eTVzdqtP0kmh7DwgHaHa?w=186&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7',
+    //         [new Ingredient('Ketchup',1)]
+    //     )
+    // ];
+    
+    private recipes: Recipe[] = []
 
     constructor(private shoppingListService: ShoppingListService) {}
 
@@ -37,7 +39,7 @@ export class RecipeService {
         this.recipes = recipes
         this.recipesChanged.next(this.recipes.slice())
     }
-    
+
     getRecipes() {
         return this.recipes.slice();
     }
