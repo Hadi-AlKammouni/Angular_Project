dea29253e767a33de5099927de92f6d887a9cbf9
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-08-27 15:08:16 +0300

    angular/Reflecting the auth state in the UI

--- angular/src/app/auth/auth.component.ts
+++ angular/src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthResounseData, AuthService } from "./auth.service";
 
@@ -13,7 +14,7 @@ export class AuthComponent {
     error = '';
     
 
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private router: Router) {}
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode
@@ -39,6 +40,7 @@ export class AuthComponent {
         authObservable.subscribe(resData => {
             console.log(resData)
             this.isLoading = false
+            this.router.navigate(['/recipes'])
         }, errorMessage => {
             console.log(errorMessage)
             this.error = errorMessage
