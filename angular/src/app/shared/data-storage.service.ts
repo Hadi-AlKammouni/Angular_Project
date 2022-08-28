8763df0860a33ca2b43d12b16a870b0e13c394cb
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-08-28 16:39:05 +0300

    angular/Attaching the token with an interceptor

--- angular/src/app/shared/data-storage.service.ts
+++ angular/src/app/shared/data-storage.service.ts
@@ -19,13 +19,8 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.authService.user.pipe(take(1), exhaustMap(user => {
-            return this.http.get<Recipe[]>('https://angular-recipe-book-97ee3-default-rtdb.firebaseio.com/recipes.json',
-            {
-                params: new HttpParams().set('auth', user.token)
-            })    
-        }),
-        map(recipes => {
+        return this.http.get<Recipe[]>('https://angular-recipe-book-97ee3-default-rtdb.firebaseio.com/recipes.json')
+        .pipe(map(recipes => {
             return recipes.map(recipe => {
                 return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
             })
